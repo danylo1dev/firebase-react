dedbb876dfac94729ee335d5754ab5ffd9d10caf
Danylo Hlushko <danylo.hlushko.dev@gmail.com>
2024-04-10 21:42:42 +0300

    added: cliud functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,36 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp(functions.config().firebase);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.someroute = functions.http.onRequest(async (req, res) => {
+  const { name, phone } = req.query;
+
+  const addContact = await admin
+    .firestore()
+    .collection("contacts")
+    .add({ name, phone });
+
+  req.json({ result: addContact.id });
+});
+
+exports.addDate = functions.firestore
+  .document("contacts/{contactId}")
+  .onCreate((snap, ctx) => {
+    const timestamp = admin.firestore.FieldValue.serverTimestamp();
+    return admin
+      .firestore()
+      .doc(`contacts/${ctx.params.contactId}`)
+      .update({ createAt: timestamp });
+  });
+
+exports.addLog = functions.http.onCall((data, ctx) => {
+  const log = {
+    message: data.message,
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+  };
+
+  return admin.firestore().doc(`logs`).update(log);
+});
